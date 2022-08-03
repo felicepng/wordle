8921098baadc82da645566ced922ccfe6978a8d8
Felice <felicepng2010@gmail.com>
2022-08-04 02:17:36 +0800

    feat: edit nav and favicon

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,25 @@
 <nav
-	class="bg-black text-white flex justify-center text-3xl sm:text-4xl font-bold py-3 border-b border-gray-700"
+	class="bg-animate text-transparent bg-gradient-to-r bg-clip-text from-yellow-500 via-teal-500 to-indigo-600 flex justify-center text-3xl sm:text-4xl font-semibold italic tracking-wide py-3 border-b border-gray-700"
 >
-	Wordle
+	wordle!
 </nav>
+
+<style>
+	.bg-animate {
+		background-size: 150%;
+
+		-webkit-animation: Animation 5s ease infinite;
+		-moz-animation: Animation 5s ease infinite;
+		animation: Animation 5s ease infinite;
+	}
+
+	@keyframes Animation {
+		0%,
+		100% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+	}
+</style>
